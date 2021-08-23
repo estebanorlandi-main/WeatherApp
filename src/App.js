03c2b2278228e68f83e6c4651cf07c3237e3051c
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-08-23 01:10:01 -0300

    fix: fixed config.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import cities from "./utility/city.list.json";
 import ListCities from "./components/ListCities";
 import Nav from "./components/Nav";
-import APIKEY from "../config.js"
+import API_KEY from "./config.js";
 
 function App() {
   const [toShow, setToShow] = useState([]);
@@ -11,9 +11,12 @@ function App() {
   const [suggestions, setSuggestions] = useState([]);
 
   const onSearch = (name) => {
-    fetch(`api.openweathermap.org/data/2.5/weather?q=${name}&appid=${}`).then(res => res.json()).then(data => setToShow(oldToShow => data))
-    setToShow((oldToShow) =>
-    );
+    fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${name}&appid=${API_KEY.weather}`
+    )
+      .then((res) => res.json())
+      .then((data) => setToShow((oldToShow) => data));
+    setToShow((oldToShow) => {});
   };
 
   return (
