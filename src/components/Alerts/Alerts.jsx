3824c974adbfaa081fec3a6618a2df9977cde8c7
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-08-28 16:43:46 -0300

    fix: removed alertType function

--- src/components/Alerts/Alerts.jsx
+++ src/components/Alerts/Alerts.jsx
@@ -1,13 +1,6 @@
 import React from "react";
 import "./Alerts.css";
 
-const alertType = (type) => {
-  switch (type) {
-    case "error":
-      return "error";
-  }
-};
-
 export default function Alerts({ alert, remove }) {
   return (
     <div onClick={remove} className={`alert ${alert.type}`}>
