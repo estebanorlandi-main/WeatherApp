2902e66f2bee0d3e05cd5ae6fa65bd55943aa054
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-08-27 22:31:08 -0300

    .

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 import "./City.css";
+import getIcon from "../../img/Icons/index";
 
 export default function City({ city }) {
   return (
     <div className="city">
+      <img className="bg-icon" src={getIcon(city.img)} alt="" />
       <h2 className="city__name">{city.name}</h2>
       <p className="city__desc">{city.desc}</p>
       <h1 className="city__temp">{city.temp}&deg;C</h1>
-      <div className="min-max">
-        <div className="min">
+      <div className="f-inline my-1">
+        <div className="f-col mx-1">
           <span>Min</span>
           <span>{city.min}&deg;C</span>
         </div>
-        <div className="max">
+        <div className="f-col mx-1">
           <span>Max</span>
           <span>{city.max}&deg;C</span>
         </div>
-      </div>
-
-      <div>
-        <div className="min"></div>
       </div>
     </div>
   );
