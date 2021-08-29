e2e2ad92517ff92594e9fb751ff9a5b2abdae5d4
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-08-29 16:13:17 -0300

    feat: little about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "./About.css";
 
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className="container">
+      <h1>How am I?</h1>
+      <p className="text">I am Esteban Orlandi from Buenos Aires, Argentina</p>
+      <h1>Why?</h1>
+      <p className="text">
+        The purpose of this page is learn about React and handle an API request.
+        Everything in here is maded by me, from the icons to the code.
+      </p>
+    </div>
+  );
 }
