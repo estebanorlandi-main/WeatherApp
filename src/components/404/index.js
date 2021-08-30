fdc56e9a7625299a1e73499119c7fcdb24c01b2b
Esteban Orlandi <estebanorlandi4@gmail.com>
2021-08-30 12:01:47 -0300

    feat: 404

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,4 +1,16 @@
 import React from "react";
+import icon from "../../img/Icons/not_found.svg";
+
 export default function NotFound(props) {
-  return <h1>Hola</h1>;
+  return (
+    <img
+      style={{
+        display: "block",
+        width: "10em",
+        margin: "3em auto",
+      }}
+      src={icon}
+      alt="Not found"
+    />
+  );
 }
